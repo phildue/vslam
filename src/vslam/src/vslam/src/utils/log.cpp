bce8c06b4e250bbbffce2cc79df716b36f31d004
--global <phild@protonmail.com>
2023-05-27 11:01:05 +0000

    recreate log directory

--- src/vslam/src/vslam/src/utils/log.cpp
+++ src/vslam/src/vslam/src/utils/log.cpp
@@ -1,11 +1,14 @@
 #include "core/types.h"
 #include "log.h"
 INITIALIZE_EASYLOGGINGPP
-
+#include <filesystem>
+namespace fs = std::filesystem;
 namespace vslam::log
 {
 void initialize(const std::string & folder)
 {
+  fs::remove_all(folder);
+  fs::create_directories(folder);
   el::Loggers::reconfigureAllLoggers(
     el::ConfigurationType::Filename, format("{}/vslam.log", folder));
 }
